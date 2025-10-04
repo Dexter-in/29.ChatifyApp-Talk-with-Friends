e844255785262bf6f4916aea551b7b1f02b62787
jeffboriz <99708349+Dexter-in@users.noreply.github.com>
2025-10-04 22:07:18 +0530

    signup implemention

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,9 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 // Imports the message routes from the message.route.js file.
 
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+
+import cors from "cors";
 
 
 
@@ -34,7 +36,8 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());//req.body is used to get data from username and password 
-app.use(cookieParser())
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
